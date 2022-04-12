5b2b87fcfb8108d36373d4574c891adeee45e98e
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-11 21:33:56 -0700

    added forecast and time

--- assets/java.js
+++ assets/java.js
@@ -11,6 +11,9 @@ var btnEl=$('#btn');
 var titleEl = $('#dailyWeather');
 var localTempEl = $('#localTemp');
 var humidityEL = $('#humidity');
+var windSpeedEl = $('#windSpeed');
+var uviEl = $('#uvi');
+var searchLog = [];
 var cityName;
 var humidity;
 var windSpeed;
@@ -19,7 +22,7 @@ var temp;
 function getLocation(event){
     event.preventDefault();    
     city = $('#search').val().trim();
-    saveSearch();
+    saveSearch(city);
     if(city){
         // put city in api endpoint
         getWeather(city);
@@ -31,6 +34,19 @@ function getLocation(event){
 
 };
 
+function getUviforecast(lat,lon){
+    var url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon='+ lon + '&exclude=hourly,minutely,alert&appid=774227cba4191819abc929f4c68a7098';
+    fetch(url).then(response=>{
+        if(response.ok){
+            response.json().then(data=>{
+               
+                var uviEl = document.getElementById('uvi');
+                uviEl.textContent = 'uvi  ' + data.current.uvi; 
+            })
+        }
+    })
+}
+
 function getWeather(city){
 
     var weather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=774227cba4191819abc929f4c68a7098';
@@ -40,12 +56,14 @@ function getWeather(city){
         if(response.ok){
             response.json().then(data =>{
                 weatherData = data;
+                console.log(weatherData);
                 temp = weatherData.main.temp;
                 humidity = weatherData.main.humidity;
                 windSpeed = weatherData.wind.speed;
                 cityName = weatherData.name;
+                getUviforecast(weatherData.coord.lat, weatherData.coord.lon);
                
-                cityDisplay(data);
+                cityDisplay(weatherData);
 
             });
 
@@ -55,27 +73,40 @@ function getWeather(city){
 
 
 
-function cityDisplay(data){
+function cityDisplay(weatherData){
+    var unixTime = moment.unix(weatherData.dt).format('MM/DD/YYYY');
+
     // displays city name
     var cityTitle = document.createElement('h1');
-    cityTitle.textContent = cityName;
+    cityTitle.textContent = cityName +' '+ unixTime;
+
 
     titleEl.append(cityTitle);
+
     // displays local temp
     var displayTemp = document.createElement('p');
     displayTemp.textContent = 'temperature  ' + temp;
 
     localTempEl.append(displayTemp);
+
     // display local humidity
     var localHumidity = document.createElement('p');
     localHumidity.textContent = 'humidity  ' + humidity;
-    console.log(humidity)
+    
+    humidityEL.append(localHumidity);
+
+    // display wind speed
+    var localWind = document.createElement('p');
+    localWind.textContent = 'Wind Speed  ' + windSpeed;
+
+    windSpeedEl.append(localWind);
 
 
 
 };
-function saveSearch(){
-    localStorage.setItem('city',city);
+function saveSearch(city){
+    searchLog.push(city);
+    localStorage.setItem('searchLog',searchLog);
 };
 
 $('#btn').on('click',getLocation);
